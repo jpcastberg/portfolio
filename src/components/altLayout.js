b0d93195ea79c8355227b4742c4e9e874af4ba70
John Castberg <jpcastberg@gmail.com>
2019-06-25 20:34:32 -0700

    Add About page and alt layout

---
+++ src/components/altLayout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default ({ children }) => (
+  <div>
+    Hello From Layout
+    {children}
+  </div>
+);
---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import AltLayout from '../components/altLayout';
+
+export default () => (
+  <AltLayout>
+    <div>Hello World</div>
+  </AltLayout>
+);
