59dbefff15d7db53998d4f87f016fa58d8df68a2
John Castberg <jpcastberg@gmail.com>
2019-06-25 23:36:28 -0700

    Add Projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import AltLayout from '../components/altLayout';
+
+export default () => (
+  <AltLayout currentPage="Projects">
+    <div>Hello World from Projects</div>
+  </AltLayout>
+);
