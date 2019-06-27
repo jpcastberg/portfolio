a8c7a9e9db642be3936e0bfb10434303e0541c1d
John Castberg <jpcastberg@gmail.com>
2019-06-27 11:28:05 -0700

    Move Nav to dedicated file, implement Nav animation

---
+++ src/components/Nav.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { Link } from 'gatsby';
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+const NavArea = styled.div`
+  position: sticky;
+  top: 0px;
+  width: 100%;
+  height: 50px;
+  border-bottom: thin solid white;
+  visibility: ${props => props.out ? 'hidden' : 'visible'};
+  animation: ${props => props.out ? fadeOut : fadeIn} .3s linear;
+  transition: visibility .3s linear;
+`;
+
+const NavContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  width: 50%;
+  max-width: 500px;
+  min-width: 300px;
+  height: 100%;
+  margin: 0px auto;
+`;
+
+const NavLink = styled(Link)`
+  color: white;
+`;
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      out: null,
+    }
+  }
+
+  componentDidMount() {
+    const { props: { location: { pathname } } } = this;
+    if (pathname === '/') {
+      this.setState({ out: true });
+    } else if (pathname !== '/') {
+      this.setState({ out: false });
+    }
+  }
+  
+  componentDidUpdate(prevProps) {
+    const { state: { out }, props: { location: { pathname: currentPage } } } = this;
+    const { location: { pathname: prevPage } } = prevProps;
+    if (prevPage === '/' && currentPage !== '/' && out === true) {
+      this.setState({ out: false });
+    } else if (prevPage !== '/' && currentPage === '/' && out === false) {
+      this.setState({ out: true });
+    }
+  }
+
+  createNavItem(navDestination) {
+    const { props: { location: { pathname } } } = this;
+    let userFacingNavText;
+    if (navDestination === '/') {
+      userFacingNavText = 'Main';
+    } else {
+      userFacingNavText = `${navDestination[1].toUpperCase()}${navDestination.slice(2)}`
+    }
+    if (navDestination === pathname) {
+      return (
+        <span style={{ cursor: 'default' }}>{userFacingNavText}</span>
+      );
+    } else {
+      return (
+        <NavLink to={navDestination}>
+          {userFacingNavText}
+        </NavLink>
+      )
+    }
+  }
+
+  render() {
+    const { state: { out } } = this;
+    return (
+      <NavArea out={out}>
+        <NavContainer>
+            <li>
+              {this.createNavItem('/')}
+            </li>
+            <li>
+              {this.createNavItem('/about')}
+            </li>
+            <li>
+              {this.createNavItem('/projects')}
+            </li>
+        </NavContainer>
+      </NavArea>
+    )
+  }
+}
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,72 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
 import ContentWrapper from '../components/ContentWrapper';
 import Transition from '../components/Transition';
-
-const NavArea = styled.div`
-  position: sticky;
-  top: 0px;
-  width: 100%;
-  height: 50px;
-  border-bottom: thin solid white;
-`;
-
-const NavContainer = styled.ul`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  list-style: none;
-  width: 50%;
-  max-width: 500px;
-  min-width: 300px;
-  height: 100%;
-  margin: 0px auto;
-`;
-
-const NavLink = styled(Link)`
-  color: white;
-`;
-
-const Nav = ({ location }) => (
-  <NavArea>
-    <NavContainer>
-        <li>
-          {createNavItem('/', location)}
-        </li>
-        <li>
-          {createNavItem('/about', location)}
-        </li>
-        <li>
-          {createNavItem('/projects', location)}
-        </li>
-    </NavContainer>
-  </NavArea>
-);
-
-const createNavItem = (navDestination, location) => {
-  const { pathname } = location;
-  let userFacingNavText;
-  if (navDestination === '/') {
-    userFacingNavText = 'Main';
-  } else {
-    userFacingNavText = `${navDestination[1].toUpperCase()}${navDestination.slice(2)}`
-  }
-  if (navDestination === pathname) {
-    return (
-      <span style={{ cursor: 'default' }} >{userFacingNavText}</span>
-    );
-  } else {
-    return (
-      <NavLink to={navDestination}>
-        {userFacingNavText}
-      </NavLink>
-    )
-  }
-}
+import Nav from '../components/Nav';
 
 export default ({ children, location, location: { pathname } }) => (
   <div>
@@ -77,8 +14,8 @@ export default ({ children, location, location: { pathname } }) => (
         { name: `keywords`, content: `web, software, development, portfolio, San Francisco` },
       ]}
     />
+    <Nav location={location} />
     <Transition location={location}>
-      {pathname === '/' ? null : <Nav location={location} />}
       <ContentWrapper main={pathname === '/'} >
         {children}
       </ContentWrapper>
