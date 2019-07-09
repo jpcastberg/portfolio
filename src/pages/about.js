ab7e64cef9b56716907b08101f6062ec594ebb79
John Castberg <jpcastberg@gmail.com>
2019-07-09 13:05:30 -0700

    Update About page with dummy content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import faker from 'faker';
 
-import ContentWrapper from '../components/ContentWrapper';
+import Heading from '../components/Heading'
+
+const Content = styled.div`
+  position: relative;
+  top: 50px;
+  width: 90%;
+  margin: 0px auto;
+`;
+
+const contentGenerator = (paragraphs) => {
+  let content = '';
+  for (let i = 0; i < paragraphs; i += 1) {
+    content += faker.lorem.paragraphs();
+  }
+  return content;
+}
 
 export default () => (
-  <div>
-    <div>
-      ABOUT Outside content wrapper
-    </div>
-    <ContentWrapper>
-      <div>This is the content within Content Wrapper</div>
-    </ContentWrapper>
-  </div>
+    <Content>
+      <Heading>About</Heading>
+      {contentGenerator(20)}
+    </Content>
 );
