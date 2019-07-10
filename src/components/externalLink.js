e8142800b3d49ed2baa7c9ec7a8a2618d64e83c4
John Castberg <jpcastberg@gmail.com>
2019-07-10 16:32:29 -0700

    Implement AirBnB style on External Link

--- src/components/externalLink.js
+++ src/components/externalLink.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import Img from 'gatsby-image';
 
-export default ({ fixed, href }) => (
+const ExternalLink = ({ fixed, href }) => (
   <li>
     <a href={href} target="_blank" rel="noopener noreferrer">
       <Img fixed={fixed} />
     </a>
   </li>
 );
+
+ExternalLink.propTypes = {
+  fixed: propTypes.shape({
+    base64: propTypes.string,
+    height: propTypes.number,
+    src: propTypes.string,
+    srcSet: propTypes.string,
+    width: propTypes.number,
+  }),
+  href: propTypes.string,
+};
+
+ExternalLink.defaultProps = {
+  fixed: {},
+  href: '',
+};
+
+export default ExternalLink;
