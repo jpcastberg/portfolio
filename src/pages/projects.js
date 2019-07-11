b69a9575b5d4c7813bc3b0b96de45665d1fd44e0
John Castberg <jpcastberg@gmail.com>
2019-07-11 09:33:52 -0700

    Implement basic formatting on 'Projects' page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import ContentWrapper from '../components/ContentWrapper';
+import { Heading } from '../components/Headings';
+
 const Projects = () => (
-  <div>
-    <div>PROJECTS Outside content wrapper</div>
-    <div>This is the content within Content Wrapper</div>
-  </div>
+  <ContentWrapper>
+    <Heading>Projects</Heading>
+    <div>
+      <div>PROJECTS Outside content wrapper</div>
+      <div>This is the content within Content Wrapper</div>
+    </div>
+  </ContentWrapper>
 );
 
 export default Projects;
