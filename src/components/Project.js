9a8a90024fe2a6966057fcd1b3d2bc5ca49c4dc2
John Castberg <jpcastberg@gmail.com>
2019-07-11 19:25:52 -0700

    Implement basic project structure

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,30 +3,70 @@ import styled from 'styled-components';
 import Img from 'gatsby-image';
 
 const ProjectArea = styled.div`
+  width: 90%;
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr;
+  grid-template-rows: 30px repeat(2, auto);
+  grid-template-areas:
+    "img title title"
+    "img info info"
+    "links . .";
+  justify-items: center;
   border: thin solid white;
-  width: 70%;
   padding: 10px;
   margin: 10px auto;
 `;
 
-const ProjectName = styled.h4`
+const ProjectName = styled.h3`
+  grid-area: title;
   text-align: center;
 `;
 
 const ProjectImg = styled(Img)`
-  display: inline-block;
+  grid-area: img;
 `;
 
-const ProjectDescription = styled.p`
-  display: inline-block;
+const ProjectInformation = styled.div`
+  grid-area: info;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
   text-align: center;
 `;
 
-const Project = ({ name, fixed, githubURL, liveURL, children }) => (
+const ProjectLinks = styled.div`
+  grid-area: links;
+  text-align: center;
+  margin: 5px 0px;
+`;
+
+const Project = ({ name, fixed, description, techUsed, links }) => (
   <ProjectArea>
     <ProjectName>{name}</ProjectName>
     <ProjectImg fixed={fixed} />
-    <ProjectDescription>{children}</ProjectDescription>
+    <ProjectInformation>
+      <p>{description}</p>
+      <p>
+      Tech Used:&nbsp;
+        {techUsed.map((tech, idx) => (
+          <span>{`${tech}${idx !== techUsed.length - 1 ? ', ' : ''}`}</span>
+        ))}
+      </p>
+    </ProjectInformation>
+    <ProjectLinks>
+      {links.map((link, idx) => {
+        const linkElement = (<a href={link.url} target="_blank" rel="noopener noreferrer">{link.displayName}</a>);
+        if (idx !== links.length - 1) {
+          return (
+            <span>
+              {linkElement}
+              <span>&nbsp;|&nbsp;</span>
+            </span>
+          );
+        }
+        return linkElement;
+      })}
+    </ProjectLinks>
   </ProjectArea>
 );
 
