238a11a6f89893c23e38388ced924b47e2dffd37
John Castberg <jpcastberg@gmail.com>
2019-07-13 11:48:56 -0700

    Add City Clothing Supply project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -53,6 +53,13 @@ export const query = graphql`
         }
       }
     }
+    city_clothing_supply: file(relativePath: { eq: "images/city_clothing_supply.png" }) {
+      childImageSharp {
+        fixed(width: 220) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
   }
 `;
 
