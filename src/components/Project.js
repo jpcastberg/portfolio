3d2cfa628a7e9dc597ecb3192839b41e9cc6ae9a
John Castberg <jpcastberg@gmail.com>
2019-07-13 23:40:43 -0700

    Improve styling on project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,12 +7,13 @@ const ProjectArea = styled.div`
   @media screen and (min-width: 601px) {
     display: grid;
     grid-template-columns: 1fr 2fr;
-    grid-template-rows: 30px repeat(2, auto);
+    grid-template-rows: 30px auto 35px;
     grid-template-areas:
       "img title"
       "img info"
-      "links .";
+      "links tech";
     justify-items: center;
+    height: 250px;
   }
   @media screen and (max-width: 600px) {
     display: flex;
@@ -24,32 +25,39 @@ const ProjectArea = styled.div`
   width: 85%;
   border: thin solid #ffffff;
   border-radius: 10px;
-  padding: 25px;
+  padding: 10px 15px;
   margin: 10px auto;
+  text-align: center;
 `;
 
 const ProjectName = styled.h3`
   grid-area: title;
-  text-align: center;
+  align-self: start;
 `;
 
 const ProjectImg = styled(Img)`
   grid-area: img;
+  align-self: center;
 `;
 
-const ProjectInformation = styled.div`
+const Description = styled.div`
   grid-area: info;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  text-align: center;
+  align-self: center;
+  width: 95%;
+`;
+
+const TechUsed = styled.div`
+  grid-area: tech;
+  align-self: end;
+  width: 95%;
+  &::before {
+    content: "Tech Used: "
+  }
 `;
 
 const ProjectLinks = styled.div`
-  @media screen and (min-width: 601px){
-    margin-top: 10px;
-  }
   grid-area: links;
+  align-self: end;
   text-align: center;
 `;
 
@@ -58,16 +66,14 @@ const Project = ({ name, fixed, description, techUsed, links }) => (
   <ProjectArea>
     <ProjectName>{name}</ProjectName>
     <ProjectImg fixed={fixed} />
-    <ProjectInformation>
-      <p>{description}</p>
-      <br />
-      <p>
-      Tech Used:&nbsp;
-        {techUsed.map((tech, idx) => (
-          <span key={tech}>{`${tech}${idx !== techUsed.length - 1 ? ', ' : ''}`}</span>
-        ))}
-      </p>
-    </ProjectInformation>
+    <Description>
+      {description}
+    </Description>
+    <TechUsed>
+      {techUsed.map((tech, idx) => (
+        <span key={tech}>{`${tech}${idx !== techUsed.length - 1 ? ', ' : ''}`}</span>
+      ))}
+    </TechUsed>
     <ProjectLinks>
       {links.map((link, idx) => {
         const linkElement = (<a key={link.url} href={link.url} target="_blank" rel="noopener noreferrer">{link.displayName}</a>);
